49a860e8217415e7a019afadf112fdab25375822
Mobinafshari <mma137813781378@gmail.com>
2025-05-30 13:17:32 +0330

    fix: tree shakable fixed

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,27 +1,37 @@
 import { useLoaderData, useNavigate } from 'react-router';
-import {Button} from 'custom-raui/button';
 import { useTheme } from 'custom-raui/theme';
+import { Button, Sheet, SettingsSheet, Checkbox, Switch } from 'custom-raui';
+// import { SettingsSheet } from 'custom-raui/settingsSheet';
+// import { SettingsSheet} from '@mojtaba118/raui'
 import { useState } from 'react';
 // import { Button, Checkbox, Switch } from '@mui/material';
 function Main() {
   const navigate = useNavigate();
+  const [checked, setChecked] = useState(false);
   const { users } = useLoaderData();
   const { changeTheme, theme } = useTheme();
   const [open, setOpen] = useState(false);
   return (
     <div>
-      <Button onClick={() => navigate('/test')}>Open</Button>
+      <Button onClick={() => setOpen(true)}>Open</Button>
       <h1>Main</h1>
       <ul>
         {users.map((user) => (
           <>
             <li key={user.id}>{user.name}</li>
-            {/* <Checkbox />
-            <Switch /> */}
+            <Checkbox />
+            <Switch
+              checked={checked}
+              activeColor="red"
+              onChange={(isChecked) => setChecked(isChecked)}
+            />
           </>
         ))}
       </ul>
-      {/* <Sheet open={open} onClose={() => setOpen(false)}>
+      <Sheet open={open} onClose={() => setOpen(false)}>
+        Hello
+      </Sheet>
+      <Sheet open={open} onClose={() => setOpen(false)}>
         Hello
       </Sheet>
       <SettingsSheet
@@ -29,7 +39,7 @@ function Main() {
         onClose={() => setOpen(false)}
         open={open}
         theme={theme}
-      /> */}
+      />
     </div>
   );
 }
